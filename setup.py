8e5ad003494cb3809412a3d53d4d0a0c623d0f2a
Javier Rojas <javierrojas14.jr@gmail.com>
2024-06-18 10:32:02 -0400

    Inicializa repositorio

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Discharges analysis of the "Hospital Luis Calvo Mackenna"',
+    author='Javier Rojas',
+    license='MIT',
+)
